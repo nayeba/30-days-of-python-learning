0602bb982ecee55179f4f249741b08dcf8dc600f
Nayeba Nawab <nayeba2501@gmail.com>
2026-07-25 01:03:42 +0530

    Complete Day 1 exercises

---
+++ day1.py
@@ -0,0 +1,43 @@
+print(3+4)#add
+print(3-4)#sub
+print(3*4)#multiply
+print(3/4)#divwhereit printsquotient
+print(3//4)#floordivroundsdowntonearestinteger
+print(3%4)#modulusprintsremainder
+print(3**4)#power
+print("Esther")
+print("India")
+print('I am enjoying 30 days of Python')
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Nayeba"))
+print(type("India"))
+a=2
+b=3.1
+c=4+7j
+print("examples of numbers in python:", a, b, c)
+print("This is an example of boolean ",a > b)
+print("And this is a string!")
+student = {
+    "name": "Nayeba",
+    "age": 19,
+    "course": "B.Sc Mathematics"
+}
+
+print(student)#dictionary
+fruits = ["banana", "orange", "mango", "lemon"]#list
+vegetables = ("Tomato", "Potato", "Cabbage", "Onion", "Carrot")#tuple
+flowers = {"Rose", "Lily", "Tulip", "Daisy"}#set
+print(fruits)
+print(vegetables)
+print(flowers)
+#euclidean distance
+x1=2
+y1=3
+x2=10
+y2=8
+dist=((x2-x1)**2 + (y2-y1)**2)**0.5
+print("The Euclidean distance is:", dist)
